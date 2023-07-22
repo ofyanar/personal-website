950ae4d64b6bcdb859a3259160fc2c20e9edd1fa
ofyanar <o.f.yanar@hotmail.com>
2023-07-22 18:21:10 +0100

    firebase setup

--- my-website/src/index.js
+++ my-website/src/index.js
@@ -5,6 +5,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDS2xFCkCsyl0nFOMU9grl3UXdP9xJUjbY",
+  authDomain: "omeryanar-1b0a4.firebaseapp.com",
+  projectId: "omeryanar-1b0a4",
+  storageBucket: "omeryanar-1b0a4.appspot.com",
+  messagingSenderId: "479440665135",
+  appId: "1:479440665135:web:19260b9ce171dde54764da",
+  measurementId: "G-PX0N1SWQ0P"
+
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
